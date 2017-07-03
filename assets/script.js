34b21dd8ebf0dbefdf757d22fb0bf5c59efdc13e
Andy Bonner <abonner@duke.edu>
2017-07-03 12:02:40 -0400

    add gitignore

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+// specify globals for the benefit of ESLint
+/* global $, firebase */
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyAHoO8GAcHnwCdbuPxfY-w1peDZl744qcA",
@@ -8,3 +11,57 @@ var config = {
   messagingSenderId: "683009294904"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // ...
+// });
+
+// firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // ...
+// });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log(user);
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  console.log(result);
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
