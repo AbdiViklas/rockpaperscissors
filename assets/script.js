1c9c65607323ecf959ccc504ae338e4b1eec0a8f
Andy Bonner <abonner@duke.edu>
2017-07-03 08:44:52 -0400

    initialize frameworks

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAHoO8GAcHnwCdbuPxfY-w1peDZl744qcA",
+  authDomain: "rockpaperscissors-fbf7f.firebaseapp.com",
+  databaseURL: "https://rockpaperscissors-fbf7f.firebaseio.com",
+  projectId: "rockpaperscissors-fbf7f",
+  storageBucket: "",
+  messagingSenderId: "683009294904"
+};
+firebase.initializeApp(config);
